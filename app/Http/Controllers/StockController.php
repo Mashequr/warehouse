27b9957d64072f1cd4966338eff87ea51cbabf62
Mashequr Khan <83307230+Mashequr@users.noreply.github.com>
2021-05-04 00:37:03 +0600

    Add files via upload

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -151,10 +151,22 @@ class StockController extends Controller
 
     public function confirmdischarge(Request $request)
     {
-        //dd($request->input());
-        DB::table('invoices')->where('stockid', $request->stockid)->update(['latefee'=>$request->latefee]);
-        DB::table('stocks')->where('stockid', $request->stockid)->update(['discharge_date'=>$request->discharge_date,'status'=>'Discharged']);
-        return redirect(route('admin_dischargedstocks'))->with('success','Stock Discharged Successfully');
+
+        try{
+            //dd($request->input());
+            DB::table('invoices')->where('stockid', $request->stockid)->update(['latefee'=>$request->latefee]);
+            DB::table('stocks')->where('stockid', $request->stockid)->update(['discharge_date'=>$request->discharge_date,'status'=>'Discharged']);
+
+            DB::table('stocks')->where('stockid', $request->stockid)->delete();
+            DB::table('invoices')->where('stockid', $request->stockid)->delete();
+            // return redirect(route('admin_viewstocks'))->with('success', 'Stock Deleted Successfully');
+
+            return redirect(route('admin_dischargedstocks'))->with('success','Stock Discharged Successfully');
+        }
+        catch(Exception $e)
+        {
+            return redirect(route('admin_viewstocks'))->with('failed', 'Operation Error !!!');
+        }
 
     }
 
